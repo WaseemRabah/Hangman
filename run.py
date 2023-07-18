bab34f1c1a88fa66f53b70ccab087c22cea52ca9
waseem Rabah <wasee.96.ra@gmail.com>
2023-07-18 14:23:30 +0200

    Add input function

--- run.py
+++ run.py
@@ -12,24 +12,3 @@ print("Welcome to Hangman !")
 print(' '.join(hidden_word))
 #create guessed_letter that take the value from the user.
 guessed_letter = input("guess a letter:").lower()
-game_over = False
-#create a loob to check if the guessed letter in the random word.
-while not game_over and lives > 0:
-    guessed_letter = input("guess a letter:").lower()
-    if guessed_letter in random_word:
-        for i in range(len(random_word)):
-            if random_word[i] == guessed_letter:
-                hidden_word[i] = guessed_letter
-    else:
-        lives -= 1
-
-    print(' '.join(hidden_word))
-    print("Lives:", lives)
-
-    if '_' not in hidden_word:
-        print("Congratulations! You guessed the word correctly!")
-        game_over = True
-    if lives == 0:
-        print("Game over! You ran out of lives.")
-        print("The word was:", random_word)
-        game_over = True
