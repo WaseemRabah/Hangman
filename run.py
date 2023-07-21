249bea1e262df4a6d7cd95c571bc89fc819f7b47
waseem Rabah <wasee.96.ra@gmail.com>
2023-07-21 05:43:11 +0200

    Add hangman instructions

--- run.py
+++ run.py
@@ -2,61 +2,49 @@ import random
 from words import fruits
 from stages import hangman_stages
 
+"""display the Hangman menu options"""
 def display_menu():
-    print("Hangman Menu:")
-    print("1. Instructions")
-    print("2. Play")
-    print("3. Quit")
+    print("Hangman Menu :")
+    print("1.Instructions")
+    print("2.Play")
+    print("3.Quit")
 
+"""display hangman instructions"""
 def display_instructions():
     print("Hangman is a word guessing game.")
-    print("A random word will be chosen, and you have to guess the letters.")
+    print("A random word will be chosen from fruits list, and you have to guess the letters.")
     print("You have 6 lives. Each incorrect guess will cost you a life.")
     print("Guess all the letters in the word to win!")
 
-def play_hangman():
-    random_word = random.choice(fruits).lower()
-    hidden_word = ['_'] * len(random_word)
-    lives = 6
-    print("Welcome to Hangman!")
+#create random_word that store the chosen word from fruits list.
+random_word = random.choice(fruits).lower()
+print(random_word)
+#we do not want the user to see the word,so we hide it. 
+hidden_word = ['_'] * len(random_word)
+lives = 6
+#welcome message.
+print("Welcome to Hangman !")
+print(' '.join(hidden_word))
+game_over = False
+#create a loob to check if the guessed letter in the random word.
+while not game_over and lives > 0:
+    guessed_letter = input("guess a letter:\n").lower()
+    if guessed_letter in random_word:
+        for i in range(len(random_word)):
+            if random_word[i] == guessed_letter:
+                hidden_word[i] = guessed_letter
+    else:
+        lives -= 1
+        print(hangman_stages[6 - lives])
+
     print(' '.join(hidden_word))
-    game_over = False
+    print("Lives:", lives)
 
-    while not game_over and lives > 0:
-        guessed_letter = input("Guess a letter: ").lower()
-        if guessed_letter in random_word:
-            for i in range(len(random_word)):
-                if random_word[i] == guessed_letter:
-                    hidden_word[i] = guessed_letter
-        else:
-            lives -= 1
-            print(hangman_stages[6 - lives])
+    if '_' not in hidden_word:
+        print("Congratulations! You guessed the word correctly!")
+        game_over = True
+    if lives == 0:
+        print("Game over! You ran out of lives.")
+        print("The word was:", random_word)
+        game_over = True
 
-        print(' '.join(hidden_word))
-        print("Lives:", lives)
-
-        if '_' not in hidden_word:
-            print("Congratulations! You guessed the word correctly!")
-            game_over = True
-        if lives == 0:
-            print("Game over! You ran out of lives.")
-            print("The word was:", random_word)
-            game_over = True
-
-def main():
-    while True:
-        display_menu()
-        choice = input("Enter your choice (1, 2, or 3): ")
-        
-        if choice == '1':
-            display_instructions()
-        elif choice == '2':
-            play_hangman()
-        elif choice == '3':
-            print("Goodbye! Thanks for playing.")
-            break
-        else:
-            print("Invalid choice. Please choose again.")
-
-if __name__ == "__main__":
-    main()
